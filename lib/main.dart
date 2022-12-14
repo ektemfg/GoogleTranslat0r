edebe256f522acc31974df4f8671685bd6bd180c
ektemfg <ekteg@icloud.com>
2022-12-14 04:24:03 +0100

    Fixed Favorite button icon is out of sync with favorite button in recent searches Fixed - CANT ADD SAME TRANSLATION TWICE IN HISTORY

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'App.dart';
 
@@ -10,6 +11,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Google Translator',
       theme: ThemeData(
